47cd3d01cecced3e6821ab896a02b27f8bad4971
Paul G. Gottschalk III <pgottschalk@epsilon-imaging.com>
2025-12-06 23:09:07 -0500

    Though most of Ch 1

--- PaulsStepByStep/Chapter01.py
+++ PaulsStepByStep/Chapter01.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
 showfigs = True
-showfig1 = True
+showfig1 = False
+showfig2 = False
 
 # Synthetic data generation
 #
@@ -45,4 +46,258 @@ if showfigs and showfig1:
     ax2.set_title('Generated Data = Validation')
     plt.show()
 
-    i = 0
+# Gradient Descent
+#
+
+# Step 0 - Initializes parameters "b" and "w" randomly
+np.random.seed(42)
+b = np.random.randn(1)
+w = np.random.randn(1)
+
+print(b, w)
+
+# Step 1 - Computes our model's predicted output - forward pass
+yhat = b + w * x_train
+
+# Step 2 - Computing the loss
+# We are using ALL data points, so this is BATCH gradient
+# descent. How wrong is our model? That's the error!
+error = (yhat - y_train)
+
+# It is a regression, so it computes mean squared error (MSE)
+loss = (error ** 2).mean()
+
+print(loss)
+
+# Step 3 - Computes gradients for both "b" and "w" parameters
+b_grad = 2 * error.mean()
+w_grad = 2 * (x_train * error).mean()
+print(b_grad, w_grad)
+
+# Sets learning rate - this is "eta" ~ the "n" like Greek letter
+lr = 0.1
+print(b, w)
+
+# Step 4 - Updates parameters using gradients and 
+# the learning rate
+b = b - lr * b_grad
+w = w - lr * w_grad
+
+print(b, w)
+
+# Linear regression in Numpy
+#
+
+# Step 0 - Initializes parameters "b" and "w" randomly
+np.random.seed(42)
+b = np.random.randn(1)
+w = np.random.randn(1)
+
+print(b, w)
+
+# Sets learning rate - this is "eta" ~ the "n"-like Greek letter
+lr = 0.1
+
+# Defines number of epochs
+n_epochs = 1000
+
+for epoch in range(n_epochs):
+    # Step 1 - Computes model's predicted output - forward pass
+    yhat = b + w * x_train
+    
+    # Step 2 - Computes the loss
+    # We are using ALL data points, so this is BATCH gradient
+    # descent. How wrong is our model? That's the error!   
+    error = (yhat - y_train)
+
+    # It is a regression, so it computes mean squared error (MSE)
+    loss = (error ** 2).mean()
+    
+    # Step 3 - Computes gradients for both "b" and "w" parameters
+    b_grad = 2 * error.mean()
+    w_grad = 2 * (x_train * error).mean()
+    
+    # Step 4 - Updates parameters using gradients and 
+    # the learning rate
+    b = b - lr * b_grad
+    w = w - lr * w_grad
+    
+print(b, w)
+
+# Sanity Check: do we get the same results as our
+# gradient descent?
+linr = LinearRegression()
+linr.fit(x_train, y_train)
+
+print(linr.intercept_, linr.coef_[0])
+
+if showfigs and showfig2:
+    fig, ax = plt.subplots()
+    ax.scatter(x_train, y_train, color='blue', label="Actual")
+    ax.scatter(x_train, yhat, color='red', label="Model")
+    ax.set_title('Model and Actual')
+    ax.set_xlabel('Years of Experience')
+    ax.set_ylabel('Salary')
+    ax.legend()
+    plt.show()
+
+# Pytorch Tensors
+#
+
+scalar = torch.tensor(3.14159)
+vector = torch.tensor([1, 2, 3])
+matrix = torch.ones((2, 3), dtype=torch.float)
+tensor = torch.randn((2, 3, 4), dtype=torch.float)
+
+print(scalar)
+print(vector)
+print(matrix)
+print(tensor)
+
+print(tensor.size(), tensor.shape)
+print(scalar.size(), scalar.shape)
+
+# We get a tensor with a different shape but it still is
+# the SAME tensor
+same_matrix = matrix.view(1, 6)
+# If we change one of its elements...
+same_matrix[0, 1] = 2.
+# It changes both variables: matrix and same_matrix
+print(matrix)
+print(same_matrix)
+
+# We can use "new_tensor" method to REALLY copy it into a new one
+different_matrix = matrix.new_tensor(matrix.view(1, 6))
+# Now, if we change one of its elements...
+different_matrix[0, 1] = 3.
+# The original tensor (matrix) is left untouched!
+# But we get a "warning" from PyTorch telling us 
+# to use "clone()" instead!
+print(matrix)
+print(different_matrix)
+
+# Lets follow PyTorch's suggestion and use "clone" method
+another_matrix = matrix.view(1, 6).clone().detach()
+# Again, if we change one of its elements...
+another_matrix[0, 1] = 4.
+# The original tensor (matrix) is left untouched!
+print(matrix)
+print(another_matrix)
+
+# Loading Data
+#
+
+x_train_tensor = torch.as_tensor(x_train)
+print(x_train.dtype, x_train_tensor.dtype)
+
+float_tensor = x_train_tensor.float()
+print(float_tensor.dtype)
+
+dummy_array = np.array([1, 2, 3])
+dummy_tensor = torch.as_tensor(dummy_array)
+# Modifies the numpy array
+dummy_array[1] = 0
+# Tensor gets modified too...
+print(dummy_tensor)
+print(dummy_tensor.numpy())
+
+# Devices and CUDA
+#
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+n_cudas = torch.cuda.device_count()
+for i in range(n_cudas):
+    print(torch.cuda.get_device_name(i))
+gpu_tensor = torch.as_tensor(x_train).to(device)
+print(gpu_tensor[0])
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Our data was in Numpy arrays, but we need to transform them 
+# into PyTorch's Tensors and then we send them to the 
+# chosen device
+x_train_tensor = torch.as_tensor(x_train).float().to(device)
+y_train_tensor = torch.as_tensor(y_train).float().to(device)
+
+# Here we can see the difference - notice that .type() is more
+# useful since it also tells us WHERE the tensor is (device)
+print(type(x_train), type(x_train_tensor), x_train_tensor.type())
+
+# The line below doesn't work because you can't go straight from a CUDA tensor to Numpy, you 
+# need to convert back to a CPU tensor first:
+#back_to_numpy = x_train_tensor.numpy()
+back_to_numpy = x_train_tensor.cpu().numpy()
+
+# Creating parameters
+#
+
+# FIRST
+# Initializes parameters "b" and "w" randomly, ALMOST as we
+# did in Numpy since we want to apply gradient descent on
+# these parameters we need to set REQUIRES_GRAD = TRUE
+torch.manual_seed(42)
+b = torch.randn(1, requires_grad=True, dtype=torch.float)
+w = torch.randn(1, requires_grad=True, dtype=torch.float)
+print(b, w)
+
+# SECOND
+# But what if we want to run it on a GPU? We could just
+# send them to device, right?
+torch.manual_seed(42)
+b = torch.randn(1, requires_grad=True, dtype=torch.float).to(device)
+w = torch.randn(1, requires_grad=True, dtype=torch.float).to(device)
+print(b, w)
+# Sorry, but NO! The to(device) "shadows" the gradient...
+
+# THIRD
+# We can either create regular tensors and send them to
+# the device (as we did with our data)
+torch.manual_seed(42)
+b = torch.randn(1, dtype=torch.float).to(device)
+w = torch.randn(1, dtype=torch.float).to(device)
+# and THEN set them as requiring gradients...
+b.requires_grad_()
+w.requires_grad_()
+print(b, w)
+
+# FINAL
+# We can specify the device at the moment of creation
+# RECOMMENDED!
+
+# Step 0 - Initializes parameters "b" and "w" randomly
+torch.manual_seed(42)
+b = torch.randn(1, requires_grad=True, \
+                dtype=torch.float, device=device)
+w = torch.randn(1, requires_grad=True, \
+                dtype=torch.float, device=device)
+print(b, w)
+
+# Autograd
+#
+
+# Step 1 - Computes our model's predicted output - forward pass
+yhat = b + w * x_train_tensor
+
+# Step 2 - Computes the loss
+# We are using ALL data points, so this is BATCH gradient descent
+# How wrong is our model? That's the error! 
+error = (yhat - y_train_tensor)
+# It is a regression, so it computes mean squared error (MSE)
+loss = (error ** 2).mean()
+
+# Step 3 - Computes gradients for both "b" and "w" parameters
+# No more manual computation of gradients! 
+# b_grad = 2 * error.mean()
+# w_grad = 2 * (x_tensor * error).mean()
+loss.backward()
+
+print(error.requires_grad, yhat.requires_grad, b.requires_grad, w.requires_grad)
+print(y_train_tensor.requires_grad, x_train_tensor.requires_grad)
+
+# Grad
+#
+
+print(b.grad, w.grad)
+
+
+i = 0
